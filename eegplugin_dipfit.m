d3d79253e364c1ff4dc6acc6f897d87ff66651c8
Arnaud Delorme <arnodelorme@gmail.com>
2022-11-18 13:12:14 -0800

    change version name:

--- eegplugin_dipfit.m
+++ eegplugin_dipfit.m
@@ -40,7 +40,7 @@
 
 function vers = eegplugin_dipfit(fig, trystrs, catchstrs)
     
-    vers = 'dipfit4.3';
+    vers = 'dipfit4.4';
     if nargin < 3
         error('eegplugin_dipfit requires 3 arguments');
     end
