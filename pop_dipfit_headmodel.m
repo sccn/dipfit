293137d5e89231d669d43bd3d185c1cbd85f5869
Arnaud Delorme <arnodelorme@gmail.com>
2022-12-10 21:50:10 -0800

    functional headmodel

--- pop_dipfit_headmodel.m
+++ pop_dipfit_headmodel.m
@@ -26,7 +26,7 @@
 % Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-function [OUTEEG, com] = pop_dipfit_headmodel( EEG, mriFile, varargin )
+function [EEG, com] = pop_dipfit_headmodel( EEG, mriFile, varargin )
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 if nargin<1
